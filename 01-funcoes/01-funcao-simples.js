09b7474e9c3c52c915e57cd89800f528f1a59cd6
RENAN BEZERRA FERNANDES MORATO <26171633@edu119.local>
2026-08-11 16:44:39 -0300

    Alterações

---
+++ 01-funcoes/01-funcao-simples.js
@@ -0,0 +1,17 @@
+function saudacao(){
+    console.log("Olá, Turma");
+}
+
+saudacao();
+
+function bomDia(){
+    console.log("Bom Dia");
+}
+
+bomDia();
+
+function apresentar(nome){
+    console.log("Olá, "+ nome);
+}
+apresentar("Renan");
+apresentar("Evellyn");
