83e889c951c500fc3d877969b379a146bdc1d12d
RENAN BEZERRA FERNANDES MORATO <26171633@edu119.local>
2026-08-18 14:00:38 -0300

    salvando alterações

---
+++ 02-metodos-arrays/02-map.js
@@ -0,0 +1,11 @@
+const numeros = [1, 2, 3, 4, 5];
+
+const dobrado = numeros.map((num) => num * 2);
+
+console.log(dobrado);
+
+let nomes = ["Brenda", "Carlos", "Diana", "Felipe"];
+
+let mensagem = nomes.map((nome) => `Olá, ${nome.toUpperCase}}`);
+
+console.log(mensagem);
