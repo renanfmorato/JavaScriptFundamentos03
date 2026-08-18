58a8ea85e4e84027859167549c668e8ccd90d1e0
RENAN BEZERRA FERNANDES MORATO <26171633@edu119.local>
2026-08-18 09:50:26 -0300

    Alterando

---
+++ 01-funcoes/05-funcao.condicional.js
@@ -0,0 +1,10 @@
+function verificaNota(media){
+    // if(media >= 7){
+    //     return "Aprovado"
+    // }
+    // return "Reprovado"
+
+    return media >= 7 ? "Aprovado" : "Reprovado"
+}
+
+console.log(verificaNota(8));
