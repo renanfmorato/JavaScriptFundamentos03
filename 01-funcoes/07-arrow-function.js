80500cb9eb860bd104326aaaa4b6052dbbe3015f
RENAN BEZERRA FERNANDES MORATO <26171633@edu119.local>
2026-08-18 11:42:52 -0300

    Alterações

---
+++ 01-funcoes/07-arrow-function.js
@@ -0,0 +1,15 @@
+function somarTradicional(a, b){
+    return a + b;
+}
+
+const somarAnonima = function (a, b){
+      return a + b;
+}
+
+const somarArrowfunction = (a,b) =>{
+    return a + b;
+}
+
+console.log(somarTradicional(10,5));
+console.log(somarAnonima(10,5));
+console.log(somarArrowfunction(10,5));
