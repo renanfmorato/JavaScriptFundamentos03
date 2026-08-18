4a59d72cf85691294fe3a3e6739341b1226c4edf
RENAN BEZERRA FERNANDES MORATO <26171633@edu119.local>
2026-08-18 09:38:57 -0300

    Alterações

---
+++ 01-funcoes/03-return.js
@@ -0,0 +1,8 @@
+function somar (a , b){
+    return a +b;
+}
+
+
+let resultado = somar(10, 5);
+
+console.log(resultado);
---
+++ 01-funcoes/04-media.js
@@ -0,0 +1,5 @@
+function calcularMedia(nota01, nota2){
+    return (nota1 + nota2) / 2;
+}
+
+console.log(calcularMedia(8, 6));
