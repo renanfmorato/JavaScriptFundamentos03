d6d0740dc3f79de569b9832f807e2bc044605991
RENAN BEZERRA FERNANDES MORATO <26171633@edu119.local>
2026-08-18 14:14:56 -0300

    Salvando Alterações

---
+++ 02-metodos-arrays/04-find.js
@@ -0,0 +1,9 @@
+const aluno = ["Giovani", "Gustavo", "Ingrid", "Isabela"];
+
+const aluno = alunos.find((nome) => nome === "Ingrid");
+
+console.log(aluno);
+
+const aluno1 = alunos.find((nome) => nome === "Felipe");
+
+console.log(aluno1);
